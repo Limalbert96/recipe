f333cebb865bd0c6cde1d0f57b6be3aefd9dce2c
limalbert12 <limalbert12@gmail.com>
2018-04-01 18:07:45 -0400

    Revert "frontend update"
    This reverts commit 33d5b6c0d961f5ea5e34a2c48a27c77271d9d5d4.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,11 +8,11 @@ import {Recipe} from '../recipe.model'; //manage array of recipes
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('Recipe 1', ' this is a test 1', 'https://static01.nyt.com/images/2016/02/16/dining/16COOKING-SALMONWITHLEEKS2/16COOKING-SALMONWITHLEEKS2-articleLarge.jpg'),
-    new Recipe('Recipe 2', ' this is a test 2', 'https://static01.nyt.com/images/2016/02/16/dining/16COOKING-SALMONWITHLEEKS2/16COOKING-SALMONWITHLEEKS2-articleLarge.jpg'),
-    new Recipe('Recipe 3', ' this is a test 3', 'https://static01.nyt.com/images/2016/02/16/dining/16COOKING-SALMONWITHLEEKS2/16COOKING-SALMONWITHLEEKS2-articleLarge.jpg'),
-    new Recipe('Recipe 4', ' this is a test 4', 'https://static01.nyt.com/images/2016/02/16/dining/16COOKING-SALMONWITHLEEKS2/16COOKING-SALMONWITHLEEKS2-articleLarge.jpg'),
-    new Recipe('Recipe 5', ' this is a test 5', 'https://static01.nyt.com/images/2016/02/16/dining/16COOKING-SALMONWITHLEEKS2/16COOKING-SALMONWITHLEEKS2-articleLarge.jpg'),
+    new Recipe('Recipe 1', ' this is a test', 'https://static01.nyt.com/images/2016/02/16/dining/16COOKING-SALMONWITHLEEKS2/16COOKING-SALMONWITHLEEKS2-articleLarge.jpg'),
+    new Recipe('Recipe 1', ' this is a test', 'https://static01.nyt.com/images/2016/02/16/dining/16COOKING-SALMONWITHLEEKS2/16COOKING-SALMONWITHLEEKS2-articleLarge.jpg'),
+    new Recipe('Recipe 1', ' this is a test', 'https://static01.nyt.com/images/2016/02/16/dining/16COOKING-SALMONWITHLEEKS2/16COOKING-SALMONWITHLEEKS2-articleLarge.jpg'),
+    new Recipe('Recipe 1', ' this is a test', 'https://static01.nyt.com/images/2016/02/16/dining/16COOKING-SALMONWITHLEEKS2/16COOKING-SALMONWITHLEEKS2-articleLarge.jpg'),
+    new Recipe('Recipe 1', ' this is a test', 'https://static01.nyt.com/images/2016/02/16/dining/16COOKING-SALMONWITHLEEKS2/16COOKING-SALMONWITHLEEKS2-articleLarge.jpg'),
   ];
   constructor() { }
 
