9a2c7b7473c908cbf286ba1e7c9ab88cec9451c7
limalbert12 <limalbert12@gmail.com>
2018-04-01 23:28:57 -0400

    Comments added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,16 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
+  
+
   loadedFeature ='recipe';
   
   onNavigate(feature: string){
     this.loadedFeature = feature;
   }
-
   
+
+
+
+   
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,22 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"]
 })
 export class HeaderComponent {
+  public isRecipe: boolean = true;
+
   @Output() featureSelected = new EventEmitter<string>(); //() create object instantiation
-  
-  onSelect(feature: string){
+
+  onSelect(feature: string) {
     this.featureSelected.emit(feature);
+
+    if (feature == "recipe") {
+      this.isRecipe = true;
+    } else {
+      this.isRecipe = false;
+    }
   }
 }
